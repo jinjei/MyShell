2e97501e6f4dd5f70e6030187ccaf57c8b5913d3
Jiaxu Jin <114793758+jinjei@users.noreply.github.com>
2025-04-02 23:51:08 -0400

    只差source subshell

--- shell.hh
+++ shell.hh
@@ -10,6 +10,8 @@ struct Shell {
   static bool isTerminal();
   static bool promptNeeded;  //标记是否需要显示提示符
   static Command _currentCommand;
+  static bool _isTerminal;
+  static std::string _shellPath; // 存储Shell可执行文件路径
 };
 
 #endif
